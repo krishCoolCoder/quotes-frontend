ef1fa6c7d12983211bbcafaa49dcfd7a626e983a
saikrishnakingtyle <77505364+saikrishnakingtyle@users.noreply.github.com>
2024-05-09 00:09:21 +0530

    feat(homePage): Implemented the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
